6be9fef947de9ba78dca616685eeaebb8b936ff4
tristankthomas <tristankthomas@icloud.com>
2023-04-20 22:59:19 +1000

    Skeleton code added

---
+++ agent/program.py
@@ -0,0 +1,43 @@
+# COMP30024 Artificial Intelligence, Semester 1 2023
+# Project Part B: Game Playing Agent
+
+from referee.game import \
+    PlayerColor, Action, SpawnAction, SpreadAction, HexPos, HexDir
+
+
+# This is the entry point for your game playing agent.
+
+class Agent:
+    def __init__(self, color: PlayerColor, **referee: dict):
+        """
+        Initialise the agent.
+        """
+        self._color = color
+        match color:
+            case PlayerColor.RED:
+                print("Testing: I am playing as red")
+            case PlayerColor.BLUE:
+                print("Testing: I am playing as blue")
+
+    def action(self, **referee: dict) -> Action:
+        """
+        Return the next action to take.
+        """
+        match self._color:
+            case PlayerColor.RED:
+                return SpawnAction(HexPos(3, 3))
+            case PlayerColor.BLUE:
+                # This is going to be invalid... BLUE never spawned!
+                return SpreadAction(HexPos(3, 3), HexDir.Up)
+
+    def turn(self, color: PlayerColor, action: Action, **referee: dict):
+        """
+        Update the agent with the last player's action.
+        """
+        match action:
+            case SpawnAction(cell):
+                print(f"Testing: {color} SPAWN at {cell}")
+                pass
+            case SpreadAction(cell, direction):
+                print(f"Testing: {color} SPREAD from {cell}, {direction}")
+                pass
